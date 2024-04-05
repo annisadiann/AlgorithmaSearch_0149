64fac314cdb645e41b644c552f4f0d912fc3ba2f
annisadiann <annisadian0412@gmail.com>
2024-04-05 10:58:13 +0700

    memasukkan perintah if

--- Algo_5April/Algo_5April.cpp
+++ Algo_5April/Algo_5April.cpp
@@ -46,6 +46,10 @@ void LinearSearch()
         {
             ctr++;
             if (arr[i] == item)
+            {
+                cout << "\n" << item << " found at position " << (i + 1) << endl; // step 6
+                break;
+            } 
         }
     }
 } 
